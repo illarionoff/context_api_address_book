5e942cf6b984452c27acbc9d549dbf3c2c8c90b3
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2018-10-10 06:36:40 -0500

    Updates

---
+++ precache-manifest.9dd7c22dfd015c7ae69b8f67650d4aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d1556cdcd4173de7d4f",
+    "url": "/context_api_address_book/static/js/runtime~main.5d1556cd.js"
+  },
+  {
+    "revision": "a18b20fc611c3195d233",
+    "url": "/context_api_address_book/static/js/main.a18b20fc.chunk.js"
+  },
+  {
+    "revision": "ea99d0dfec1ebc1de3b4",
+    "url": "/context_api_address_book/static/js/1.ea99d0df.chunk.js"
+  },
+  {
+    "revision": "a18b20fc611c3195d233",
+    "url": "/context_api_address_book/static/css/main.09e997af.chunk.css"
+  },
+  {
+    "revision": "ea99d0dfec1ebc1de3b4",
+    "url": "/context_api_address_book/static/css/1.22f844a7.chunk.css"
+  },
+  {
+    "revision": "5078ba2556c477eeecf1a3c1ded5d433",
+    "url": "/context_api_address_book/index.html"
+  }
+];
